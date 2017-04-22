d119cb14a3f7ede57c0c9841c71ae1a31cb70ce5
j4y88 <j4y88@hotmail.com>
2017-04-21 22:57:28 -0400

    Optimise R1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
-$('.main-carousel').flickity({cellAlign:'center',contain:true,wrapAround:true,});$(function(){$('a[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')&&location.hostname==this.hostname){var target=$(this.hash);target=target.length?target:$('[name='+this.hash.slice(1)+']');if(target.length){$('html, body').animate({scrollTop:target.offset().top},500);return false;}}});});function EmailCheck(inputText){var mailformat=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;if(inputText.value.match(mailformat)){alert("Thank you for signing up!");document.form1.email.focus();return true;}else{alert("You have entered an invalid email address!");document.form1.email1.focus();return false;}};$(function(){var count=0;var countText=$('#cart-counter');$('.carousel-cell button').on('click',updateCartCount);function updateCartCount(){count++;countText.html(count).show();}})
+//Flickity Carousel
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'center',
+  contain: true,
+  wrapAround: true,
+});
+
+
+//Smooth Scrolling
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});  
+
+//Email Validation Check
+function EmailCheck(inputText){  
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+  if(inputText.value.match(mailformat)){  
+    alert("Thank you for signing up!");
+    document.form1.email.focus();  
+    return true;  
+  } else {  
+    alert("You have entered an invalid email address!");  
+    document.form1.email1.focus();  
+    return false;
+  }
+};
+
+//Add Cart
+$(function() {
+  var count = 0;
+  var countText = $('#cart-counter');
+
+  $('.carousel-cell button').on('click', updateCartCount);
+
+  function updateCartCount() {
+    count++;
+    countText.html(count).show();
+  }
+})
